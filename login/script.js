66ff0e923781c4b514f1b135817ea037354b8722
victor manuel cuaresma salhuana <155250959+victormanuelcuaresmasalhuana@users.noreply.github.com>
2023-12-30 12:24:29 -0500

    1.0

---
+++ login/script.js
@@ -0,0 +1,4 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar el envío del formulario
+});
